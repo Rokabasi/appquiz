1d527d0a5dc5a6296cd3441f907d7e0f393b156c
Romain <kabasiromain@gmail.com>
2022-07-21 15:28:35 +0100

    modification couleur

--- script.js
+++ script.js
@@ -1,3 +1,170 @@
+const questions = [{
+        number: 1,
+        question: "En quelle année a été créé JavaScript?",
+        answer: "1996",
+        options: [
+            "1996",
+            "1990",
+            "2001",
+            "1999",
+
+        ]
+    },
+    {
+        number: 2,
+        question: "Quel est le type d'un fichier JavaScript?",
+        answer: ".js",
+        options: [
+            ".css",
+            ".javascript",
+            ".jsx",
+            ".js"
+        ]
+    },
+    {
+        number: 3,
+        question: "Laquelle de ces déclarations est incorrecte",
+        answer: "const name = 2",
+        options: [
+            "let a = 1",
+            "const game = 'football'",
+            "const name = 2",
+            "let nom"
+        ]
+    },
+    {
+        number: 4,
+        question: "Quel selecteur utilisé pour selectionné un élement id du DOM en js",
+        answer: "document.getElementsById",
+        options: [
+            "document.takeElementById",
+            "document.getElementsById",
+            "document.catchElementById",
+            "document.getId",
+        ]
+    },
+    {
+        number: 5,
+        question: "Quelle propriété renvoie la taille d'une chaine",
+        answer: "length",
+        options: [
+            "size",
+            "taille",
+            "mysize",
+            "length"
+        ]
+    },
+    {
+        number: 6,
+        question: "Où mettons-nous notre javascript?",
+        answer: "<body>",
+        options: [
+            "<html>",
+            "<img>",
+            "<javascript>",
+            "<body>"
+        ]
+    },
+    {
+        number: 7,
+        question: 'Comment pouvez-vous écrire "Hello W3docs" avec alerte?',
+        answer: 'alert("Hello W3docs");',
+        options: [
+            'alertBox("Hello W3docs");',
+            'alert("Hello W3docs");',
+            'msgBox("Hello W3docs");',
+            'msg("Hello W3docs");'
+        ]
+    },
+    {
+        number: 8,
+        question: "Comment appeler la fonction myFunction en JavaScript?",
+        answer: "myFunction(...)",
+        options: [
+            "funcall myFunction(...)",
+            "call function myFunction(...)",
+            "myFunction(...)",
+            "call myFunction(...)"
+        ]
+    },
+    {
+        number: 9,
+        question: "Est-ce que JavaScript est le même que Java?",
+        answer: "non",
+        options: [
+            "oui",
+            "non",
+            "oui, sauf le nom",
+            "pas vraiment"
+        ]
+    },
+    {
+        number: 10,
+        question: "Quellle est la syntaxe correcte?",
+        answer: "i += 1;",
+        options: [
+            "i =+ 1;",
+            "i += 1;",
+            "i = i++1",
+            "+i+"
+        ]
+    },
+    {
+        number: 11,
+        question: "i = 0; i +=2; Que vaut i",
+        answer: "2",
+        options: [
+            "0",
+            "rien va s'éxecuter",
+            "2",
+            "aucune bonne réponse"
+        ]
+    },
+    {
+        number: 12,
+        question: "Quelle méthode du DOM n'existe pas",
+        answer: "document.getElementByclass",
+        options: [
+            "document.querySelectorAll",
+            "document.querySelector",
+            "document.getElementByclass",
+            "document.getElementsById"
+        ]
+    },
+    {
+        number: 13,
+        question: "Quel attribut des noeuds du DOM correspond à l'attribut HTML class?",
+        answer: "className",
+        options: [
+            "class",
+            "MyClass",
+            "className",
+            "byMyclassName"
+        ]
+    },
+    {
+        number: 14,
+        question: "Lequel de ces événements n'existe pas",
+        answer: "mousepass",
+        options: [
+            "load",
+            "mouseclick",
+            "mouseout",
+            "mousepass"
+        ]
+    },
+    {
+        number: 15,
+        question: "Qui fondé JavaScript",
+        answer: "Brendan Eich",
+        options: [
+            "Mark Zuckerberg",
+            "Steve Jobs",
+            "Bill Gates",
+            "Brendan Eich"
+        ]
+    },
+];
 const container = document.querySelector('.container');
 const question_box = document.querySelector('.question-box');
 const result_box = document.querySelector('.result-box');
@@ -85,19 +252,12 @@ function validateInputs() {
 
 function showQuestion(index) {
     removecolor();
-
     question.textContent = questions[index].question;
     question_number.textContent = "Question " + questions[index].number + "/15";
     label1.textContent = questions[index].options[0];
     label2.textContent = questions[index].options[1];
     label3.textContent = questions[index].options[2];
     label4.textContent = questions[index].options[3];
-    // for (i = 0; i < 3; i++) {
-    //     option[i].setAttribute("onclick", "optionSelected(this)");
-    //     console.log(option)
-    // }
-
-
 }
 
 function monscore(dutext) {
@@ -112,6 +272,7 @@ function monscore(dutext) {
         mychoice = false;
         makechoice = false;
     }
+    console.log(score);
 }
 for (let i = 0; i < 4; i++) {
     input_answer[i].addEventListener("change", () => {
@@ -129,7 +290,6 @@ for (let i = 0; i < 4; i++) {
             monscore(questions[numero].options[3]);
         }
         firstanwser.classList.add("decoration");
-
     })
 }
 
@@ -146,9 +306,6 @@ function removecolor() {
 function uploadradio() {
     for (let i = 0; i < 4; i++) {
         input_answer[i].checked = false;
-        input_answer[i].checked = false;
-        input_answer[i].checked = false;
-        input_answer[i].checked = false;
     }
     btn_next.disabled = true;
 }
@@ -157,9 +314,7 @@ function myprogressebar() {
     sec = 60;
     const timesecond = document.querySelector('.time-left');
     const timer = setInterval(() => {
-
         timesecond.innerHTML = sec--;
-
         progressbar.style.width = `${(sec/60)*100}%`
         if (sec == -1) {
             nextquestion();
@@ -176,7 +331,7 @@ btn_next.addEventListener('click', e => {
 })
 
 function nextquestion() {
-    makechoice = false;
+
     mychoice = false;
     sec = 60;
     clearInterval(sec);
@@ -216,7 +371,6 @@ function result() {
         source = "win.png";
     }
     let resultat = `
-
         <div class="result-name">${gamername}</div>
         <div class="result-email">${gameremail}</div>
         <div class="result-icone"><img src="${source}" alt="result" class="img-result"></div><br>
