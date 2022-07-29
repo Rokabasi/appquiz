9d3d7a3783c7285eebcb70f2fa9c43fad7716b12
Romain <kabasiromain@gmail.com>
2022-07-29 11:16:03 +0100

    integration des feedback

--- script.js
+++ script.js
@@ -1,205 +1,35 @@
-const questions = [{
-        number: 1,
-        question: "En quelle année a été créé JavaScript?",
-        answer: "1996",
-        options: [
-            "1996",
-            "1990",
-            "2001",
-            "1999",
-
-        ]
-    },
-    {
-        number: 2,
-        question: "Quel est le type d'un fichier JavaScript?",
-        answer: ".js",
-        options: [
-            ".css",
-            ".javascript",
-            ".jsx",
-            ".js"
-        ]
-    },
-    {
-        number: 3,
-        question: "Laquelle de ces déclarations est incorrecte",
-        answer: "const name = 2",
-        options: [
-            "let a = 1",
-            "const game = 'football'",
-            "const name = 2",
-            "let nom"
-        ]
-    },
-    {
-        number: 4,
-        question: "Quel selecteur utilisé pour selectionné un élement id du DOM en js",
-        answer: "document.getElementsById",
-        options: [
-            "document.takeElementById",
-            "document.getElementsById",
-            "document.catchElementById",
-            "document.getId",
-        ]
-    },
-    {
-        number: 5,
-        question: "Quelle propriété renvoie la taille d'une chaine",
-        answer: "length",
-        options: [
-            "size",
-            "taille",
-            "mysize",
-            "length"
-        ]
-    },
-    {
-        number: 6,
-        question: "Où mettons-nous notre javascript?",
-        answer: "<body>",
-        options: [
-            "<html>",
-            "<img>",
-            "<javascript>",
-            "<body>"
-        ]
-    },
-    {
-        number: 7,
-        question: 'Comment pouvez-vous écrire "Hello W3docs" avec alerte?',
-        answer: 'alert("Hello W3docs");',
-        options: [
-            'alertBox("Hello W3docs");',
-            'alert("Hello W3docs");',
-            'msgBox("Hello W3docs");',
-            'msg("Hello W3docs");'
-        ]
-    },
-    {
-        number: 8,
-        question: "Comment appeler la fonction myFunction en JavaScript?",
-        answer: "myFunction(...)",
-        options: [
-            "funcall myFunction(...)",
-            "call function myFunction(...)",
-            "myFunction(...)",
-            "call myFunction(...)"
-        ]
-    },
-    {
-        number: 9,
-        question: "Est-ce que JavaScript est le même que Java?",
-        answer: "non",
-        options: [
-            "oui",
-            "non",
-            "oui, sauf le nom",
-            "pas vraiment"
-        ]
-    },
-    {
-        number: 10,
-        question: "Quellle est la syntaxe correcte?",
-        answer: "i += 1;",
-        options: [
-            "i =+ 1;",
-            "i += 1;",
-            "i = i++1",
-            "+i+"
-        ]
-    },
-    {
-        number: 11,
-        question: "i = 0; i +=2; Que vaut i",
-        answer: "2",
-        options: [
-            "0",
-            "rien va s'éxecuter",
-            "2",
-            "aucune bonne réponse"
-        ]
-    },
-    {
-        number: 12,
-        question: "Quelle méthode du DOM n'existe pas",
-        answer: "document.getElementByclass",
-        options: [
-            "document.querySelectorAll",
-            "document.querySelector",
-            "document.getElementByclass",
-            "document.getElementsById"
-        ]
-    },
-    {
-        number: 13,
-        question: "Quel attribut des noeuds du DOM correspond à l'attribut HTML class?",
-        answer: "className",
-        options: [
-            "class",
-            "MyClass",
-            "className",
-            "byMyclassName"
-        ]
-    },
-    {
-        number: 14,
-        question: "Lequel de ces événements n'existe pas",
-        answer: "mousepass",
-        options: [
-            "load",
-            "mouseclick",
-            "mouseout",
-            "mousepass"
-        ]
-    },
-    {
-        number: 15,
-        question: "Qui fondé JavaScript",
-        answer: "Brendan Eich",
-        options: [
-            "Mark Zuckerberg",
-            "Steve Jobs",
-            "Bill Gates",
-            "Brendan Eich"
-        ]
-    },
-];
+import { questions } from './question.js';
 const container = document.querySelector('.container');
-const question_box = document.querySelector('.question-box');
-const result_box = document.querySelector('.result-box');
+const questionContainer = document.querySelector('.question-box');
+const resultContainer = document.querySelector('.result-box');
 const form = document.querySelector('#form');
-const nom = document.getElementById('name');
-const email = document.getElementById('email');
-const label1 = document.querySelector('#answer1');
-const label2 = document.querySelector('#answer2');
-const label3 = document.querySelector('#answer3');
-const label4 = document.querySelector('#answer4');
-const input_container = document.querySelectorAll(".answer");
+const inputName = document.getElementById('name');
+const inputEmail = document.getElementById('email');
+const firstLabel = document.querySelector('#answer1');
+const secondLabel = document.querySelector('#answer2');
+const thirdLabel = document.querySelector('#answer3');
+const fourthLabel = document.querySelector('#answer4');
 const question = document.querySelector('.question');
-const question_number = document.querySelector('.question-number');
-const block1 = document.querySelectorAll('#question1');
-const input_answer = document.querySelectorAll('.question-input');
-const firstanwser = document.querySelector('#question1');
-const secondtanwser = document.querySelector('#question2');
-const thirdanwser = document.querySelector('#question3');
-const fourthanwser = document.querySelector('#question4');
-const btn_next = document.querySelector('.next');
+const questionNumber = document.querySelector('.question-number');
+const inputAnswer = document.querySelectorAll('.question-input');
+const firstAnswer = document.querySelector('#question1');
+const secondAnswer = document.querySelector('#question2');
+const thirdAnswer = document.querySelector('#question3');
+const fourthAnswer = document.querySelector('#question4');
+const btnNext = document.querySelector('.next');
 let progressbar = document.querySelector('.progress-bar');
-let numero = 0;
-let makechoice = false;
-let mychoice = false;
-let sec;
-let useranswer;
-let score;
-let questcompteur;
+let number = 0;
+let makeChoice = false;
+let myChoice = false;
+let sec = 60;
+let score = 0;
+let quit = document.querySelector('#quit');
 form.addEventListener('submit', event => {
     event.preventDefault();
-    myprogressebar();
+    myProgessebar();
     validateInputs();
-    showQuestion(numero);
-    numero = 0;
-    questcompteur = 0;
+    showQuestion(number);
+    number = 0;
     score = 0;
 });
 
@@ -213,147 +43,140 @@ function setError(element, message) {
     inputControl.classList.remove('sucess');
 }
 
-function setSuccess(element) {
-    const inputControl = element.parentElement;
-    const errorDisplay = inputControl.querySelector('.error-message')
-    errorDisplay.innerText = '';
-    inputControl.classList.add('success');
-    inputControl.classList.remove('error');
-    element.classList.add('success');
-    element.classList.remove('errors');
-
-}
-
 function isValidEmail(email) {
-    const re = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
+    const myRegex = /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return myRegex.test(String(email).toLowerCase());
 }
 
 function validateInputs() {
-    const usernameValue = nom.value.trim();
-    const emailValue = email.value.trim();
-    const container = document.querySelector('.container');
+    const usernameValue = inputName.value.trim();
+    const emailValue = inputEmail.value.trim();
     if (usernameValue === '' && emailValue === '') {
-        setError(nom, "N'oubliez pas de renseigner votre nom avant de commencer le quiz!");
-        setError(email, "N'oubliez pas de renseigner votre nom avant de commencer le quiz!");
+        setError(inputName, "N'oubliez pas de renseigner votre inputName avant de commencer le quiz!");
+        setError(inputEmail, "N'oubliez pas de renseigner votre inputName avant de commencer le quiz!");
     } else if (usernameValue === '') {
-        setError(nom, "N'oubliez pas de renseigner votre nom avant de commencer le quiz!");
+        setError(inputName, "N'oubliez pas de renseigner votre inputName avant de commencer le quiz!");
     } else if (usernameValue.length < 3) {
-        setError(nom, "Votre nom doit contenir au moins troi caractères!");
+        setError(inputName, "Votre inputName doit contenir au moins troi caractères!");
     } else if (emailValue === '') {
-        setError(email, "N'oubliez pas de renseigner votre nom avant de commencer le quiz!");
+        setError(inputEmail, "N'oubliez pas de renseigner votre inputName avant de commencer le quiz!");
     } else if (!isValidEmail(emailValue)) {
-        setError(email, "renseigner un mail correct");
+        setError(inputEmail, "renseigner un mail correct");
     } else {
         container.classList.add('hide');
-        question_box.classList.remove('hide');
+        questionContainer.classList.remove('hide');
     }
 }
 
 function showQuestion(index) {
-    removecolor();
+    removeColor();
     question.textContent = questions[index].question;
-    question_number.textContent = "Question " + questions[index].number + "/15";
-    label1.textContent = questions[index].options[0];
-    label2.textContent = questions[index].options[1];
-    label3.textContent = questions[index].options[2];
-    label4.textContent = questions[index].options[3];
+    questionNumber.textContent = "Question " + questions[index].number + "/15";
+    firstLabel.textContent = questions[index].options[0];
+    secondLabel.textContent = questions[index].options[1];
+    thirdLabel.textContent = questions[index].options[2];
+    fourthLabel.textContent = questions[index].options[3];
 }
 
-function monscore(dutext) {
-    const affiche = dutext;
-    const goodanswer = questions[numero].answer;
-    if ((affiche === goodanswer) && (makechoice === false) && (mychoice === false)) {
+function monScore(dutext) {
+    const userAnswer = dutext;
+    const goodAnswer = questions[number].answer;
+    if ((userAnswer === goodAnswer) && (makeChoice === false) && (myChoice === false)) {
         score += 1;
-        makechoice = true;
-        mychoice = true;
-    } else if ((affiche != questions[numero].answer) && (makechoice == true) && (mychoice == true)) {
+        makeChoice = true;
+        myChoice = true;
+    } else if ((userAnswer != questions[number].answer) && (makeChoice == true) && (myChoice == true)) {
         score -= 1;
-        mychoice = false;
-        makechoice = false;
+        myChoice = false;
+        makeChoice = false;
     }
-    console.log(score);
 }
 for (let i = 0; i < 4; i++) {
-    input_answer[i].addEventListener("change", () => {
-        btn_next.disabled = false;
-        changecolor();
-        if (firstanwser.checked == true) {
-            monscore(questions[numero].options[0]);
-        } else if (secondtanwser.checked == true) {
-            monscore(questions[numero].options[1]);
+    inputAnswer[i].addEventListener("change", () => {
+        btnNext.disabled = false;
+        changeColor();
+        if (firstAnswer.checked == true) {
+            monScore(questions[number].options[0]);
+        } else if (secondAnswer.checked == true) {
+            monScore(questions[number].options[1]);
 
-        } else if (thirdanwser.checked == true) {
-            monscore(questions[numero].options[2]);
+        } else if (thirdAnswer.checked == true) {
+            monScore(questions[number].options[2]);
 
-        } else if (fourthanwser.checked == true) {
-            monscore(questions[numero].options[3]);
+        } else if (fourthAnswer.checked == true) {
+            monScore(questions[number].options[3]);
         }
-        firstanwser.classList.add("decoration");
+        if (document.querySelector('.answer.decoration')) {
+            document.querySelector('.answer.decoration').classList.remove('decoration');
+        }
+        if (inputAnswer[i].checked == true) {
+            inputAnswer[i].parentElement.classList.add("decoration")
+        }
     })
 }
 
-function changecolor() {
-    btn_next.classList.add('next_checked');
-    btn_next.classList.remove("next");
+function changeColor() {
+    btnNext.classList.add('next_checked');
+    btnNext.classList.remove("next");
 }
 
-function removecolor() {
-    btn_next.classList.remove('next_checked');
-    btn_next.classList.add("next");
+function removeColor() {
+    btnNext.classList.remove('next_checked');
+    btnNext.classList.add("next");
 }
 
-function uploadradio() {
+function uploadRadio() {
     for (let i = 0; i < 4; i++) {
-        input_answer[i].checked = false;
+        inputAnswer[i].checked = false;
+        inputAnswer[i].parentElement.classList.remove("decoration");
     }
-    btn_next.disabled = true;
+    btnNext.disabled = true;
 }
 
-function myprogressebar() {
+function myProgessebar() {
     sec = 60;
-    const timesecond = document.querySelector('.time-left');
+    const timeSecond = document.querySelector('.time-left');
     setInterval(() => {
-        timesecond.innerHTML = sec--;
+        timeSecond.innerHTML = sec--;
         progressbar.style.width = `${(sec/60)*100}%`
         if (sec == -1) {
             nextquestion();
         };
     }, 1000);
-    if ((sec == 0) && (numero == 15)) {
+    if ((sec === 0) && (number === 15)) {
         result();
     }
 }
 
-btn_next.addEventListener('click', e => {
+btnNext.addEventListener('click', e => {
     e.preventDefault();
     nextquestion()
 })
 
 function nextquestion() {
 
-    mychoice = false;
+    myChoice = false;
     sec = 60;
     clearInterval(sec);
-    numero += 1;
-    makechoice = false
-    if (numero == 14) {
-        btn_next.value = 'Terminer';
+    number += 1;
+    makeChoice = false
+    if (number === 14) {
+        btnNext.value = 'Terminer';
     }
-    if (numero < 15) {
-        showQuestion(numero);
-        uploadradio();
+    if (number < 15) {
+        showQuestion(number);
+        uploadRadio();
     }
-    if (numero == 15) {
+    if (number === 15) {
         result();
-        btn_next.addEventListener('click', e => {
+        btnNext.addEventListener('click', e => {
             e.preventDefault();
             result();
         });
     }
 }
 
-let quit = document.querySelector('#quit');
+
 quit.addEventListener('click', e => {
     e.preventDefault();
     clearInterval(sec);
@@ -362,29 +185,25 @@ quit.addEventListener('click', e => {
 
 function result() {
     clearInterval(sec);
-    const gamername = nom.value;
-    const gameremail = email.value;
-    let source;
+    const playerName = inputName.value;
+    const playerEmail = inputEmail.value;
+    let srcImage = "";
     if (score < 8) {
-        source = "lost.webp";
+        srcImage = "lost.webp";
     } else {
-        source = "win.png";
+        srcImage = "win.png";
     }
     let resultat = `
-        <div class="result-name">${gamername}</div>
-        <div class="result-email">${gameremail}</div>
-        <div class="result-icone"><img src="${source}" alt="result" class="img-result"></div><br>
+        <div class="result-name">${playerName}</div>
+        <div class="result-email">${playerEmail}</div>
+        <div class="result-icone"><img src="${srcImage}" alt="result" class="img-result"></div><br>
         <div class="result"> ${score}/15</div><br>
-        <input type="button" value="Accueil" id="accueil-button" Onclick="accueil()">
+        <input type="button" value="Accueil" id="accueil-button" onclick="location.reload()">
     `
-    question_box.classList.add('hide');
-    result_box.innerHTML = resultat;
-    result_box.classList.remove('hide');
+    questionContainer.classList.add('hide');
+    resultContainer.innerHTML = resultat;
+    resultContainer.classList.remove('hide');
     form.reset();
-    const errormess = document.querySelector('.error-message');
-    errormess.innerHTML = '';
-}
-
-function accueil() {
-    location.reload();
+    const errorMessage = document.querySelector('.error-message');
+    errorMessage.innerHTML = '';
 }
